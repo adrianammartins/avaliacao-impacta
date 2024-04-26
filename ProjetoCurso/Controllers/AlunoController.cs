7700dc5f393206de0862723c07e1467d57a8df20
Adriana Machado Martins Heck <adriana.martins.heck@avanade.com>
2024-04-26 17:48:40 -0300

    Desenvolvimento inicial do projeto

---
+++ ProjetoCurso/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoCurso.Controllers
+{
+    public class AlunoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
